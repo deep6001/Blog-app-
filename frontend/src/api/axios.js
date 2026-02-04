2b3f3dd7bf4ca8ebefd1b3e0adc3993da5a9d054
Deep Patel <pateldeep120404@gmail.com>
2026-02-04 15:32:22 +0530

    frontend hardcode url from backend fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.VITE_BASE_URL}/api` ,
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api` ,
   withCredentials: true,
 });
 
