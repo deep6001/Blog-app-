3d97110190031109523086f0372229731cad6593
Deep Patel <pateldeep120404@gmail.com>
2026-02-04 15:51:14 +0530

    add make change in cookie

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -54,7 +54,6 @@ export const loginUser = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
         res.cookie('token', token, {
              httpOnly: true,
-             sameSite: "strict",
              secure: process.env.NODE_ENV === "production",
              maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
              });
